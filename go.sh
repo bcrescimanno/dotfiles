11a4328e63e4acfa82a19c98c2cbc3fe51d88fa5
Brian Crescimanno <brian.crescimanno@gmail.com>
2012-07-12 15:27:07 -0700

    Add go.sh

---
+++ go.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e 
+
+STARTDIR=$(pwd)
+REPO="git://github.com/bcrescimanno/dotfiles.git"
+NAME="bc-dotfiles"
+INSTALLDIR=$HOME/$NAME
+
+# Clone the git repo
+git clone $REPO $INSTALLDIR
+
+# Create the backup and undo directories
+mkdir -p $INSTALLDIR/vim/backup
+mkdir -p $INSTALLDIR/vim/undo
+
+# Create symlinks
+ln -s $INSTALLDIR/jshintrc $HOME/.jshintrc
+ln -s $INSTALLDIR/vim $HOME/.vim
+ln -s $INSTALLDIR/gvimrc $HOME/.gvimrc
+ln -s $INSTALLDIR/vimrc $HOME/.vimrc
+ln -s $INSTALLDIR/tmux.conf $HOME/.tmux.conf
+
+cd $INSTALLDIR
+git submodule update --init
+cd $STARTDIR
