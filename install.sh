aac78880f4eae9b1f5dc75ed819e970c5057990d
Brian Crescimanno <bcrescimanno@paypal.com>
2014-05-07 09:54:14 -0700

    write the symlinks properly

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+HERE=$(pwd)
+
 # Create necessary subdirectories
 mkdir -p vim/backup
 mkdir -p vim/undo
 mkdir -p vim/bundle
 
 # Create symlinks
-ln -s jshintrc $HOME/.jshintrc
-ln -s vim $HOME/.vim
-ln -s gvimrc $HOME/.gvimrc
-ln -s vimrc $HOME/.vimrc
-ln -s tmux.conf $HOME/.tmux.conf
-ln -s ackrc $HOME/.ackrc
+ln -s $HERE/jshintrc $HOME/.jshintrc
+ln -s $HERE/vim $HOME/.vim
+ln -s $HERE/gvimrc $HOME/.gvimrc
+ln -s $HERE/vimrc $HOME/.vimrc
+ln -s $HERE/tmux.conf $HOME/.tmux.conf
+ln -s $HERE/ackrc $HOME/.ackrc
 
 # Download Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
