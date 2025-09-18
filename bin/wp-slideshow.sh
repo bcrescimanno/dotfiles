04434aa94d22e2bfab7751c0eaa511d68ca87232
Brian Crescimanno <brian.crescimanno@me.com>
2025-09-17 17:46:25 -0700

    Making the wallpaper script work better

--- bin/wp-slideshow.sh
+++ bin/wp-slideshow.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/sh
+#!/bin/bash
 
-# Slideshow directory
-DIR="$HOME/Pictures/4kwp/"
+DIR="${1:-$HOME/Pictures/4kwp}"
 
-# Get a random Wallpaper
-WP=$(find "$DIR" -type f | shuf -n 1)
+WP=$(find "$DIR" -type f \( \
+  -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o \
+  -iname "*.gif" -o -iname "*.bmp" -o -iname "*.webp" -o \
+  -iname "*.tiff" \
+\) | shuf -n 1)
 
-# Set the wallpaper
-hyprctl hyprpaper reload ,"$WP"
+if [[ -z "$WP" ]]; then
+    echo "No image files found in '$DIR'"
+    exit 1
+else
+    echo "$WP"
+    hyprctl hyprpaper reload ,"$WP"
+fi
