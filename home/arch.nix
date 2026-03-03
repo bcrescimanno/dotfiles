64baf234d0f2eafa0b6216a714f4b73d7d862538
Brian Crescimanno <brian.crescimanno@me.com>
2026-03-03 07:46:11 -0800

    fixing arch.nix structure for a second time

--- home/arch.nix
+++ home/arch.nix
@@ -1,6 +1,6 @@
   # The archlinux OMZ plugin provides arch helpers
   # and pacman aliases that aren't relevant on non-arch systems
-  { ... }
+  { ... };
 
   {
     programs.zsh.oh-my-zsh.plugins = [ "archlinux" ];
